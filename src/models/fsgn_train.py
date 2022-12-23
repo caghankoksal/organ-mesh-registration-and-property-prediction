ea99bfc986dcb5c6f659c05fe8c8fccc42ffe88d
Caghan <caghan.koksal@tum.de>
2022-12-23 07:03:55 +0100

    implement w&b hyperparameter search

--- src/models/fsgn_train.py
+++ src/models/fsgn_train.py
@@ -1,4 +1,5 @@
 import sys
+import wandb
 sys.path.append('/u/home/koksal/organ-mesh-registration-and-property-prediction/')
 
 import torch
@@ -8,7 +9,7 @@ from torch_geometric.data import DataLoader
 from tqdm import tqdm
 from time import sleep
 import mlflow
-from baseline_model import GCN
+from baseline_model import GNN
 import argparse
 
 def train(net, train_data, optimizer, loss_fn, device):
@@ -105,6 +106,118 @@ def test(net, train_data, test_data, device):
     return train_acc, test_acc
 
 
+def build_optimizer(network, optimizer, learning_rate):
+    if optimizer == "sgd":
+        optimizer = torch.optim.SGD(network.parameters(),
+                              lr=learning_rate, momentum=0.9)
+    elif optimizer == "adam":
+        optimizer = torch.optim.Adam(network.parameters(),
+                               lr=learning_rate)
+    return optimizer
+
+
+def build_network(configs):
+    if configs.model=='fsgnet':
+
+        model_params = dict(
+        use_input_encoder = configs.use_input_encoder,
+        num_classes=configs.num_classes,
+        in_features=configs.in_features, 
+        encoder_features=configs.enc_feats,
+        conv_channels=[256, 256, 256, 256],
+        encoder_channels=[configs.enc_feats],
+        decoder_channels=[256],
+        num_heads=num_heads,
+        apply_batch_norm=True,  
+    ) 
+
+        net = MeshSeg(**model_params)
+
+    elif configs.model=='baseline':
+        
+        print('Baseline Model is initialized')
+        model_params = dict(
+
+        hidden_channels=configs.hidden_channels,
+        layer = configs.layer,
+        use_input_encoder = configs.use_input_encoder,
+        input_encoder_dim = configs.enc_feats,
+        num_layers = configs.num_layers,
+        )
+        net = GNN(**model_params)
+
+    return net
+
+
+def build_dataset(config):
+        # Build Dataset
+    root = '/vol/chameleon/projects/mesh_gnn/organ_meshes'
+    basic_feat_path = '/vol/chameleon/projects/mesh_gnn/basic_features.csv'
+    bridge_path = '/vol/chameleon/projects/mesh_gnn/Bridge_eids_60520_87802.csv'
+    split_path = '/u/home/koksal/organ-mesh-registration-and-property-prediction/data/'
+
+    train_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path, num_samples=config.num_train_samples, mode='train', split_path=split_path )
+    val_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path,  num_samples=config.num_test_samples, mode='val', split_path=split_path )
+    test_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path,  num_samples=config.num_test_samples, mode='test', split_path=split_path )
+    train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=config.batch_size,  shuffle=False)
+
+    return train_loader, test_loader
+
+def training_function(config=None):
+    print('Input cfg to training function ',config)
+    
+    # note that we define values from `wandb.config` instead of 
+    # defining hard values
+    config = wandb.config
+    print('Training function config ',config)
+    device = config.device
+
+    #Data Loader
+    train_loader, test_loader = build_dataset(config)
+
+    #Network
+    net = build_network(config).to(device)
+    
+    #Optimizer
+    optimizer = build_optimizer(net, config.optimizer, config.lr)
+
+    #Loss Function
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+
+    best_test_acc = 0
+
+    with tqdm(range(config.max_epoch), unit="Epoch") as tepochs:
+        for epoch in tepochs:
+            train_loss = train(net, train_loader, optimizer, loss_fn, device)
+            val_loss = calculate_val_loss(net, test_loader, loss_fn, device)
+            #mlflow.log_metric('train_loss',train_loss)
+            #mlflow.log_metric('val_loss',val_loss)
+            wandb.log({'train_loss': train_loss, 'val_loss':val_loss, 'epoch': epoch})
+            train_acc, test_acc = test(net, train_loader, test_loader, device)
+            #mlflow.log_metric('train_acc',train_acc)
+            #mlflow.log_metric('test_acc',test_acc)
+            wandb.log({'train_acc': train_acc, 'test_acc':test_acc, 'epoch': epoch})
+
+            
+            tepochs.set_postfix(
+                train_loss=train_loss,
+                val_loss = val_loss,
+                train_accuracy=100 * train_acc,
+                test_accuracy=100 * test_acc,
+            )
+            sleep(0.1)
+
+            wandb.watch(net)
+
+            if test_acc > best_test_acc:
+                best_test_acc = test_acc
+                wandb.run.summary["best_test_acc"] = 100 *best_test_acc
+                wandb.run.summary["best_train_acc"] = 100 * train_acc
+                savedir = '/u/home/koksal/organ-mesh-registration-and-property-prediction/models/'
+                torch.save(net.state_dict(), f"{savedir}cenc_channels_{config.hidden_channels}_best_testacc_{test_acc:.2f}")
+
+    print('Best Test Accuracy is ',best_test_acc)
 
 
 def build_args():
@@ -127,9 +240,15 @@ def build_args():
                             help="Number of training samples")                        
 
     parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--in_features", type=int, default=3)
+    parser.add_argument("--num_classes", type=int, default=1)
     parser.add_argument("--num_layers", type=int, default=3)
+    parser.add_argument("--lr", type=float, default=0.0001)
+
     parser.add_argument("--layer", type=str, default="gcn")
+    parser.add_argument("--optimizer", type=str, default="adam")
     parser.add_argument("--use_input_encoder", type=bool, default=True)
+    parser.add_argument("--hparam_search", type=bool, default=False)
     args = parser.parse_args()
     return args
 
@@ -147,92 +266,53 @@ if __name__ == '__main__':
     num_train_samples = args.num_train_samples
     num_test_samples = args.num_test_samples
     use_input_encoder = args.use_input_encoder
-    num_layers = args.num_layers 
-    
+    num_layers = args.num_layers
+    lr = args.lr
+    hparam_search = args.hparam_search
 
-    if model=='fsgnet':
+    print('Hyperparameter  Searching  ',hparam_search)
+    if hparam_search is True:
+        print('Hyperparameter search starts')
 
-        model_params = dict(
-        use_input_encoder = use_input_encoder,
-        in_features=3,
-        encoder_features=enc_feats,
-        conv_channels=[256, 256, 256, 256],
-        encoder_channels=[enc_feats],
-        decoder_channels=[256],
-        num_classes=1,
-        num_heads=num_heads,
-        apply_batch_norm=True,  
-    )   
+        run = wandb.init(project='meshgnn-hparam-search')
 
-        net = MeshSeg(**model_params).to(device)
+        sweep_config = {
+        'method': 'random',
+        'name': 'sweep',
+        'metric': {'goal': 'maximize', 'name': 'val_acc'},
+        'parameters': 
+        {
+            'batch_size': {'value': 32},
+            'max_epoch': {'value': 50},
+            'model': {'value': 'baseline'},
+            'device': {'value': device},
+            'optimizer': {'value': 'adam'},
+            'lr': {'max': 0.1, 'min': 0.0001},
+            'num_layers': {'values': [3, 4, 5, 6, 7, 8]},
+            'use_input_encoder': {'value':True},
+            'num_train_samples': {'value':3000},
+            'num_test_samples': {'value':300},
+            'layer': {'values':['sageconv','gcn']},
+            'hidden_channels': {'values': [32, 64, 128, 256]},
+            'enc_feats': {'values': [32, 64, 128, 256, 512]},
+            'num_heads': {'values': [4, 8, 12]},
+        }
+    }
+        # 🐝 Step 3: Initialize sweep by passing in config
+        sweep_id = wandb.sweep(sweep=sweep_config, project='meshgnn-hparam-search')
+        import pprint
+        wandb.agent(sweep_id, training_function, count=10)
 
-    elif model=='baseline':
-        print('Baseline Model is initialized')
-        model_params = dict(
-        num_classes=1,
-        in_features=3, 
-        hidden_channels=args.hidden_channels,
-        layer = args.layer,
-        use_input_encoder = args.use_input_encoder,
-        input_encoder_dim = args.enc_feats,
-        num_layers = args.num_layers,
+    else:
+        print('Usual training starts')
+        run = wandb.init(
+        project="mesh_gnn_organ",
+        notes="tweak baseline",
+        tags=[ "gnn"],
+        config=args,
         )
-        net = GCN(**model_params).to(device)
-    mlflow.start_run()
-    for k,v in model_params.items():
-        mlflow.log_param(k, v)
 
-    
-
-
-    root = '/vol/chameleon/projects/mesh_gnn/organ_meshes'
-
-
-    basic_feat_path = '/vol/chameleon/projects/mesh_gnn/basic_features.csv'
-    bridge_path = '/vol/chameleon/projects/mesh_gnn/Bridge_eids_60520_87802.csv'
-    split_path = '/u/home/koksal/organ-mesh-registration-and-property-prediction/data/'
-    train_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path, num_samples=num_train_samples, mode='train', split_path=split_path )
-    val_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path,  num_samples=num_test_samples, mode='val', split_path=split_path )
-    test_dataset = OrganMeshDataset(root, basic_feat_path, bridge_path,  num_samples=num_test_samples, mode='test', split_path=split_path )
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size,  shuffle=False)
-
-    lr = 0.0001
-    num_epochs = 50
-    best_test_acc = 0.0
-
-    mlflow.log_param("lr", lr)
-    mlflow.log_param("num_epochs", num_epochs) 
-
-
-    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-    loss_fn = torch.nn.BCEWithLogitsLoss()
-
-    with tqdm(range(num_epochs), unit="Epoch") as tepochs:
-        for epoch in tepochs:
-            train_loss = train(net, train_loader, optimizer, loss_fn, device)
-            val_loss = calculate_val_loss(net, test_loader, loss_fn, device)
-            mlflow.log_metric('train_loss',train_loss)
-            mlflow.log_metric('val_loss',val_loss)
-            train_acc, test_acc = test(net, train_loader, test_loader, device)
-            mlflow.log_metric('train_acc',train_acc)
-            mlflow.log_metric('test_acc',test_acc)
-
-            
-            tepochs.set_postfix(
-                train_loss=train_loss,
-                val_loss = val_loss,
-                train_accuracy=100 * train_acc,
-                test_accuracy=100 * test_acc,
-            )
-            sleep(0.1)
-
-            if test_acc > best_test_acc:
-                best_test_acc = test_acc
-                torch.save(net.state_dict(), f"/u/home/koksal/organ-mesh-registration-and-property-prediction/models/ckpts_enc_channels_{model_params['in_features']}_best_testacc_{test_acc:.2f}")
-
-    print('Best Test Accuracy is ',best_test_acc)
+        training_function()
 
     
 
